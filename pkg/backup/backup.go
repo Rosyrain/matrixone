3c28fc8fde362b241920478a8307753186e07415
jiangxin <jx178861659@163.com>
2024-11-12 16:51:11 +0800

    update

--- pkg/backup/backup.go
+++ pkg/backup/backup.go
@@ -35,6 +35,7 @@ import (
 // Backup
 // Note: ctx needs to support cancel. The user can cancel the backup task by canceling the ctx.
 func Backup(ctx context.Context, bs *tree.BackupStart, cfg *Config) error {
+	// test
 	var err error
 	var s3Conf *s3Config
 	if !cfg.metasMustBeSet() {
